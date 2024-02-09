18959a3d21d27f6508f7b8a22837a21749020da7
alexanderokeagu <pezokeagy@gmail.com>
2024-02-09 13:45:44 +0100

    forms and user accounts

--- blog/tests.py
+++ blog/tests.py
@@ -10,14 +10,25 @@ class BlogTest(TestCase):
         self.user = get_user_model().objects.create_user(username='testuser', email='test@email.com', password='secret')
         self.post = Post.objects.create(title="a good title", body="nice body content", author=self.user)
 
+
+
     def test_string_representation(self):
         post = Post(title="a little test")
         self.assertEqual(str(post), post.title)
+
+
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(),'/post/1/' )
+
+
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'a good title')
         self.assertEqual(f'{self.post.body}', 'nice body content')
         self.assertEqual(f'{self.post.author}', 'testuser')
+
+
         
 
     def test_post_list_view(self):
@@ -27,6 +38,8 @@ class BlogTest(TestCase):
         self.assertTemplateUsed(response, 'blog/home.html')
 
 
+
+
     def test_post_detail_view(self):
         response = self.client.get('/post/1/')
         no_response = self.client.get('/post/100000/')
@@ -34,4 +47,33 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'a good title')
         self.assertTemplateUsed(response, 'blog/post_detail.html')
+
+
+
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_view'), {
+            'title': 'New title',
+            'body' : 'New text',
+            'author': self.user,
+
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+        self.assertTemplateUsed(response, 'blog/post_new.html')
+
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+           'title': 'Updated title',
+           'body' : 'Updated text', 
+        })
+        self.assertEqual(response.status_code, 302)
+        
+
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
 
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -13,3 +15,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['title', 'author','body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
